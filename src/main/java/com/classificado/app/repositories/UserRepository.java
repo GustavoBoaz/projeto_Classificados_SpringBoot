8da68e5ae623740a1b138397e2f393596fa1432c
Gustavo Boaz <gustavo.boaz@hotmail.com>
2022-01-29 11:38:26 -0300

    @GustavoBoaz [Task: ADD documentation and method findByEmail]

--- src/main/java/com/classificado/app/repositories/UserRepository.java
+++ src/main/java/com/classificado/app/repositories/UserRepository.java
@@ -1,11 +1,31 @@
 package com.classificado.app.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.classificado.app.models.User;
+import com.classificado.app.models.UserModel;
 
+/**
+ * UserRepository responsible for CRUD operations on UserModel
+ * 
+ * @author Boaz
+ * @since 26/01/2022
+ * @version 1.0
+ */
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+	
+	/**
+	 * Method responsible for find user by email
+	 * 
+	 * @author Boaz
+	 * @date 26/01/2022
+	 * @version 1.0
+	 * @param email
+	 * @return Optional<UserModel>
+	 */
+	public Optional<UserModel> findByEmail(String email);
 
 }
